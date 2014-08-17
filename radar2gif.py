677c84ffad34567043768ea03b89835aa70d28d8
Matthew Parrilla <matthew.parrilla@gmail.com>
2014-08-17 16:25:33 -0400

    Weird heroku stuff

--- radar2gif.py
+++ radar2gif.py
@@ -10,11 +10,10 @@ from StringIO import StringIO
 from twython import Twython
 #from config import APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET
 
-if not APP_KEY:
-    APP_KEY = os.environ['APP_KEY']
-    APP_SECRET = os.environ['APP_SECRET']
-    OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
-    OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
+APP_KEY = os.environ['APP_KEY']
+APP_SECRET = os.environ['APP_SECRET']
+OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
 
 
 def get_region(region_name):
@@ -22,7 +21,7 @@ def get_region(region_name):
     for radar images of specified region"""
 
     url = 'http://radar.weather.gov/ridge/Conus/RadarImg/'
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:16.0) Gecko/20100101 Firefox/16.0,gzip(gfe)'}))
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:16.0) Gecko/20100101 Firefox/16.0,gzip(gfe)'})
     soup = Soup(r.text)
 
     radar_urls = []
