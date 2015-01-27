50164625ff4f10ae8a321fc21a948322154e1ead
Matt Parrilla <matthew.parrilla@gmail.com>
2015-01-27 23:21:25 +0000

    Add bash script for cron

---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /home/mattparrilla/webapps/wxgif/app && source venv/bin/activate && /home/mattparrilla/webapps/wxgif/app/venv/bin/python2.7 radar2gif.py && rm gif/basemap/* && rm gif/source/* && rm gif/new_palette/* && deactivate
