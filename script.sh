e2bf00c67451efe423ce4d1aedb317d501148ffc
Matt Parrilla <matthew.parrilla@gmail.com>
2015-05-29 17:09:57 +0000

    Update script to remove new-projection files

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/mattparrilla/webapps/wxgif/app && source venv/bin/activate && /home/mattparrilla/webapps/wxgif/app/venv/bin/python2.7 radar2gif.py && rm gif/basemap/* && rm gif/source/* && rm gif/new_palette/* && deactivate
+cd /home/mattparrilla/webapps/wxgif/app && source venv/bin/activate && /home/mattparrilla/webapps/wxgif/app/venv/bin/python2.7 radar2gif.py && rm gif/basemap/* && rm gif/source/* && rm gif/new_palette/* && rm gif/new_projection/* && deactivate
